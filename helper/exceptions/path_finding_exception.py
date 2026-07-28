c7554a15b42a9673d983052175ed8c16e79395d8
mou-taha <t.mousnia@gmail.com>
2026-07-28 23:31:32 +0100

    Refactor data structure for pathfinding; implement disconnected zone check and update main logic

---
+++ helper/exceptions/path_finding_exception.py
@@ -0,0 +1,3 @@
+class PathFindingExeption(Exception):
+    """this class is a custom exception to handle path finding exceptions"""
+    pass
--- helper/pathfinding/path_finding.py
+++ helper/pathfinding/path_finding.py
@@ -1,20 +1,31 @@
 from models.map import Map
 from models.zone import Zone, ZoneCategory
 from typing import List
-
+from helper.exceptions.path_finding_exception import PathFindingExeption
 
 class PathFinding:
     def __init__(self, map: Map):
         self.map = map
 
-    def check_disconnected_zones(self) -> bool:
-        start_zone: Zone = [zone for zone in self.map.zones if zone.category == ZoneCategory.START_HUB][0]
+    def _check_disconnected_zones(self) -> bool:
+        start_zone: Zone = [zone for zone in self.map.zones
+                            if zone.category == ZoneCategory.START_HUB][0]
+        end_zone: Zone = [zone for zone in self.map.zones
+                          if zone.category == ZoneCategory.END_HUB][0]
         unvisited: List[Zone] = [start_zone]
         visited: list[Zone] = []
+        if not end_zone or not start_zone:
+            raise PathFindingExeption("end zone or start zone are not defined")
 
         while len(unvisited) > 0:
             current: Zone = unvisited.pop()
             visited.append(current)
             unvisited.extend([connection.zone for connection in current.connections if connection.zone not in visited and connection.zone not in unvisited])
 
-        return len(self.map.zones) == len(visited)
+        return end_zone in visited
+
+    def find_paths(self) -> List[Zone]:
+        if not self._check_disconnected_zones():
+            raise PathFindingExeption("there is no path on the given "
+                                      "map to the end zone.")
+        
