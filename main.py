d19964d44cab61336f8c55f80747b0bc65b2167b
mou-taha <t.mousnia@gmail.com>
2026-07-28 23:31:32 +0100

    Refactor data structure for pathfinding; implement disconnected zone check and update main logic

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 from helper.parser.dataParser import DataParser
 from helper.exceptions.parsingException import ParsingException
+from helper.exceptions.path_finding_exception import PathFindingExeption
 from models.map import Map
 from helper.pathfinding.path_finding import PathFinding
 
 
 def main():
     parser: DataParser = DataParser("data.txt")
-    map: Map
     try:
+        map: Map
         map = parser.parse_network_file()
-    except ParsingException as e:
-        print(e)
-        return
-    
-    print(f"Total Drones: {map.nbDrones}")
-    print(f"Total Zones Loaded: {len(map.zones)}")
+        
+        path_finding: PathFinding = PathFinding(map)
+        path_finding.find_paths()
+        print(f"Total Drones: {map.nbDrones}")
+        print(f"Total Zones Loaded: {len(map.zones)}")
 
 
-    # verify the connections and metadata worked
-    for zone in map.zones:
-        print(f"\nInspecting {zone.name}:")
-        print(f" - Coordinate: {zone.coordinate}")
-        print(f" - Type: {zone.type.name}")
-        print(f" - Color: {zone.color}")
-        print(f" - Max Drones: {zone.maxDrones}")
-        print(
-            f" - Connections: {[c.zone.name  + ' [max link capacity= ' + str(c.maxLinkCapacity) +']' for c in zone.connections]}"
-        )
-    path_finding: PathFinding = PathFinding(map)
-    print(path_finding.check_disconnected_zones())
+        # verify the connections and metadata worked
+        for zone in map.zones:
+            print(f"\nInspecting {zone.name}:") 
+            print(f" - Coordinate: {zone.coordinate}")
+            print(f" - Type: {zone.type.name}")
+            print(f" - Color: {zone.color}")
+            print(f" - Max Drones: {zone.maxDrones}")
+            print(
+                f" - Connections: {[c.zone.name  + ' [max link capacity= ' + str(c.maxLinkCapacity) +']' for c in zone.connections]}"
+            )
+    except (ParsingException, PathFindingExeption) as e:
+        print("error : ", e)
 
 
 if __name__ == "__main__":
