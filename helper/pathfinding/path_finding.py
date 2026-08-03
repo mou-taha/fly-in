3dd8c112b1be8f062f651e462964ea4e81c8f4f7
mou-taha <t.mousnia@gmail.com>
2026-08-03 22:21:14 +0100

    use currentpath list on pathfinding as visited zone

--- helper/pathfinding/path_finding.py
+++ helper/pathfinding/path_finding.py
@@ -17,7 +17,8 @@ class PathFinding:
         unvisited: List[Zone] = [start_zone]
         visited: list[Zone] = []
         if not end_zone or not start_zone:
-            raise PathFindingException("end zone or start zone are not defined")
+            raise PathFindingException("end zone or start"
+                                       " zone are not defined")
 
         while len(unvisited) > 0:
             current: Zone = unvisited.pop()
@@ -28,25 +29,6 @@ class PathFinding:
                               and connection.zone not in unvisited])
 
         return end_zone in visited
-
-    def find_paths(self) -> List[Path]:
-        # if not self._check_disconnected_zones():
-        #     raise PathFindingException("there is no path on the given "
-        #                                "map to the end zone.")
-
-        # start_zone: Zone = next((zone for zone in self.map.zones
-        #                          if zone.type == ZoneCategory.START_HUB), None)
-        # if start_zone is None:
-        #     raise PathFindingException("there is no start zone on the given"
-        #                                " map")
-
-        # paths: List[tuple[Path, bool]] = (Path([start_zone]))
-        # # iterate over the list of paths where path doesn't get to end
-        # for path in [item[0] for item in paths
-        #              if item[1] is False]:
-        #     conns = path.zones[-1].connections
-        # return paths
-        pass
 
     def get_all_possible_paths(self) -> list[Path]:
         """
@@ -70,11 +52,11 @@ class PathFinding:
 
         all_paths: List[Path] = []
 
-        def dfs(current_zone: Zone, current_path: list[Zone],
-                visited: set[str]):
+        def dfs(current_zone: Zone, current_path: list[Zone]):
             # Add the current zone to the path and mark it as visited
+            # also this list contain the visited zones in the current path
+            # to avoid cycles
             current_path.append(current_zone)
-            visited.add(current_zone.name)
 
             # If we reached the destination,
             # add a copy of the path to our results
@@ -84,15 +66,14 @@ class PathFinding:
                 # Explore all connected zones that haven't been
                 # visited in the current path
                 for neighbor in current_zone.connections:
-                    if neighbor.zone.name not in visited:
-                        dfs(neighbor.zone, current_path, visited)
+                    if neighbor.zone not in current_path:
+                        dfs(neighbor.zone, current_path)
 
             # Backtrack: remove the current zone from path and visited set
             # so it can be explored via different routes
             current_path.pop()
-            visited.remove(current_zone.name)
 
-        # Initialize the recursive search
-        dfs(start_zone, [], set())
+        # Initialize the recursive dfs function with the start zone
+        dfs(start_zone, [])
 
         return all_paths
